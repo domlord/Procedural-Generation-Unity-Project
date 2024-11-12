a3f2272a6a89bd5ddedbb3c3a30764076d31f7fe
dom lord <dominicrlord@gmail.com>
2024-11-12 22:30:34 +0000

    roomcontroller changes

--- Final Year Project Procedural Generation/Assets/Scripts/RoomController.cs
+++ Final Year Project Procedural Generation/Assets/Scripts/RoomController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RoomInfo
 {
@@ -18,7 +19,7 @@ public class RoomController : MonoBehaviour
 
     private RoomInfo CurrentLoadRoomData;
 
-    Queue<RoomInfo> _roomQueue = new Queue<RoomInfo>();
+    Queue<RoomInfo> _loadRoomQueue = new Queue<RoomInfo>();
 
     public List<Room> loadedRooms = new List<Room>();
 
@@ -27,6 +28,40 @@ public class RoomController : MonoBehaviour
     private void Awake()
     {
         Instance = this;
+    }
+
+    public void LoadRoom(string roomName, int x, int y)
+    {
+        RoomInfo newRoomData = new RoomInfo();
+        newRoomData.RoomName = roomName;
+        newRoomData.X = x;
+        newRoomData.Y = y;
+
+        _loadRoomQueue.Enqueue(newRoomData);
+    }
+
+    IEnumerator LoadRoomRoutine(RoomInfo info)
+    {
+        string roomName = CurrentRoomName + info.RoomName;
+
+        AsyncOperation loadRoom = SceneManager.LoadSceneAsync(roomName, LoadSceneMode.Additive);
+
+        while (loadRoom.isDone == false)
+        {
+            yield return null;
+        }
+    }
+
+    public void RegisterRoom(Room room)
+    {
+        room.transform.position = new Vector3(
+            CurrentLoadRoomData.X * room.Width,
+            CurrentLoadRoomData.Y * room.Height, 0);
+
+        room.X = CurrentLoadRoomData.X;
+        room.Y = CurrentLoadRoomData.Y;
+        room.name = CurrentRoomName + "-" + CurrentLoadRoomData.RoomName + "-" + room.X + ", " + room.Y;
+        room.transform.parent = transform;
     }
 
     public bool DoesRoomExist(int x, int y)
