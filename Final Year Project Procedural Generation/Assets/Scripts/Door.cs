b961b0bd364a16108d8d323a27ba07b7bfd06448
dom lord <dominicrlord@gmail.com>
2025-04-28 01:51:00 +0100

    Update Door.cs

--- Final Year Project Procedural Generation/Assets/Scripts/Door.cs
+++ Final Year Project Procedural Generation/Assets/Scripts/Door.cs
@@ -7,20 +7,23 @@ public class Door : MonoBehaviour
 {
     public enum DoorType
     {
-        Left, Right, Top, Bottom
+        Left,
+        Right,
+        Top,
+        Bottom
     }
+
     public DoorType doorType;
 
     public GameObject doorCollider;
-    
+
     private GameObject _player;
 
-    private float _widthOffset = 1.75f;
+    private const float WidthOffset = 1.75f;
 
     private void Start()
-    { 
+    {
         _player = GameObject.FindGameObjectWithTag("Player");
-        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -30,19 +33,22 @@ public class Door : MonoBehaviour
             switch (doorType)
             {
                 case DoorType.Bottom:
-                    _player.transform.position = new Vector2(_player.transform.position.x, _player.transform.position.y - _widthOffset);
+                    _player.transform.position = new Vector2(_player.transform.position.x,
+                        _player.transform.position.y - WidthOffset);
                     break;
                 case DoorType.Left:
-                    _player.transform.position = new Vector2(_player.transform.position.x - _widthOffset, _player.transform.position.y);
+                    _player.transform.position = new Vector2(_player.transform.position.x - WidthOffset,
+                        _player.transform.position.y);
                     break;
                 case DoorType.Right:
-                    _player.transform.position = new Vector2(_player.transform.position.x + _widthOffset, _player.transform.position.y);
+                    _player.transform.position = new Vector2(_player.transform.position.x + WidthOffset,
+                        _player.transform.position.y);
                     break;
                 case DoorType.Top:
-                    _player.transform.position = new Vector2(_player.transform.position.x, _player.transform.position.y + _widthOffset);
+                    _player.transform.position = new Vector2(_player.transform.position.x,
+                        _player.transform.position.y + WidthOffset);
                     break;
             }
         }
     }
 }
-
